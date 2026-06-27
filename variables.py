a82902b44ad55dee64e20551e82659e53b05281c
jyeshtha <reddyjyeshtha@gmail.com>
2026-06-27 18:33:47 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,88 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(4+5)
+9
+a=10
+print(a)
+10
+b=20
+print(b)
+20
+x=40
+print(x)
+40
+a3=80
+print(a3)
+80
+z=50
+print(z)
+50
+b123456789=300
+print(b123456789)
+300
+6a=90
+SyntaxError: invalid decimal literal
+_=100
+print(_)
+100
+if=20
+SyntaxError: invalid syntax
+a=4;b=6
+print(a+b)
+10
+a,b=3,4
+print(a,b)
+3 4
+a=2,3,4
+print(a)
+(2, 3, 4)
+a,b,c=10
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    a,b,c=10
+TypeError: cannot unpack non-iterable int object
+a=b=c=10
+print(a,b,c)
+10 10 10
+a,b,c=(2,3,4)
+print(a,b,c)
+2 3 4
+a,b,c=2,3,4,5,6,7,9
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    a,b,c=2,3,4,5,6,7,9
+ValueError: too many values to unpack (expected 3)
+first name = "pooja"
+SyntaxError: invalid syntax
+first_name = "pooja"
+print(first_name)
+pooja
+firstname="pooja"
+print(firstname)
+pooja
+first_name = "pooja"
+last_name = "reddy"
+print(first_name + last_name)
+poojareddy
+print(first_name,last_name)
+pooja reddy
+print(first_name + "" + last_name)
+poojareddy
+name="pooja"
+print(name)
+pooja
+a=6
+print(a)
+6
+del a
+print(a)
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a3'?
+name = "pooja"
+print(name)
+pooja
+NAME ="pooja"
+print(NAME)
+pooja
