c7264f01d0a13bd7489da4661100612926023603
jyeshtha <reddyjyeshtha@gmail.com>
2026-07-06 20:03:50 +0530

    Add files via upload

---
+++ runtime.py
@@ -0,0 +1,57 @@
+'''a=int(input("enter  a value"))
+b=int(input("enter b value"))
+print(a+b)'''
+
+'''a=float(input("enter a no"))
+b=float(input("enter b no"))
+print(a+b)'''
+
+'''a=str(input("enter a"))
+b=str(input("enter b"))
+print(a+b)'''
+
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+
+'''fname=input("f1 name")
+lname=input("last name")
+print((fname+" "+lname).title())'''
+
+'''a=complex(input("enter a"))
+b=complex(input("enter b"))
+print(a+b)'''
+
+'''a=bool(input("enter a"))
+b=bool(input("enter b"))
+c=bool(input("enter c"))
+print(a+b+c)'''
+
+'''a=int(input("enter a"))
+b=int(input("enter b"))
+option=int(input("choose the option add sub mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=5
+b=10
+a,b=b,a
+print(f"a={a},b={b}")'''
+
+a=float(input("value a"))
+b=float(input("value b"))
+a=a+b
+b=a-b
+a=a-b
+print(a=%.2f ,b=%.2f %(a, b))
+
+
+
+
+
+
+
+
+
+        
