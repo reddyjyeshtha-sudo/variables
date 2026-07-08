44ce2665190c49392dc485cad75bef4e5147d420
jyeshtha <reddyjyeshtha@gmail.com>
2026-07-08 19:51:45 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,141 @@
+'''a=3
+b=6
+if a<b:
+    print("true")
+else:
+    print("false")'''
+
+
+'''a=5
+b=10
+if a!=b or a==b:
+    print("yes")
+else:
+    print("no")'''
+
+'''a=5
+b=10
+if not a!=b:
+    print("yes")
+else:
+    print("no")'''
+
+
+'''a=10
+if type(a) is float:
+    print("float")
+else:
+    print("int")'''
+
+'''a=10.5
+if type(a) is not float:
+    print("float")
+else:
+    print("int")'''
+'''a=10,4,5,6
+if 20 in a:
+    print("not there")
+else:
+    print("there")'''
+
+'''a=8
+b=10
+if a<b:
+    print("great")
+elif a>b:
+    print("less")
+else:
+    print("true")'''
+
+
+'''a=10
+b=20
+if a!=b and a>=b:
+    print("true")
+elif a!=b and a<=b:
+    print("false")
+else:
+    print("ntg")'''
+
+'''a=10
+b=20
+if a!=b or a>=b:
+    print("true")
+elif a!=b and a<=b:
+    print("false")
+else:
+    print("ntg")'''
+
+'''a=10
+b=20
+if a!=b and a>=b:
+    print("true")
+elif not a!=b and a<=b:
+    print("false")
+else:
+    print("ntg")'''
+
+'''a=10
+if type(a) is int:
+    print("int")
+elif type(a) is float:
+    print("float")
+else:
+    print("str")'''
+
+'''a=10
+b=20
+if a<b:
+    print("true")
+if a!=b:
+    print("great")
+if b>a:
+    print("yes")'''
+
+'''a=4
+b=6
+if a<b:
+    print("less")
+    if b>a:
+        print("great")'''
+
+'''a=4
+b=6
+if a>b:
+    print("less")
+    if b>a:
+        print("great")'''
+
+'''a=4
+b=6
+if a<b:
+    print("less")
+    if b==a:
+        print("great")
+    else:
+        print("true")'''
+
+'''a=4
+b=6
+if a>b:
+    print("less")
+    if b>a:
+        print("great")
+else:
+    print("true")'''
+a=9
+b=11
+if a<b:
+    print("less")
+    if b==a:
+        print("great")
+    elif a!=b:
+        print("not equal")
+
+
+
+
+
+
+
+
