8806e6fc9a98dcb30d021da9531955b0dd214ff5
jyeshtha <reddyjyeshtha@gmail.com>
2026-07-09 19:55:24 +0530

    Add files via upload

---
+++ task 9-07-26.py
@@ -0,0 +1,49 @@
+'''age=int(input())
+if age>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+
+'''no=int(input())
+if no%2==0:
+    print("even")
+else:
+    print("odd")'''
+
+'''y=int(input("enter year"))
+if y%4==0:
+    print("leap yr")
+else:
+    print("not leap")'''
+
+'''name=(input("enter name"))
+if name=="pooja":
+    print("welcome pooja")
+else:
+    print("welcome  guest")'''
+
+
+
+'''username=input("enter username ")
+password=input("enter password ")
+if (username=="jyesh"):
+    if (password=="12345"):
+        print("login success")
+    else:
+        print("invalid")'''
+
+age=int(input("enter age"))
+if age>=18:
+    print("eligible for vote")
+marks=int(input("enter marks"))
+if marks>=80:
+    print("allow for scolorship")
+attendence=int(input("enter attendence"))
+if attendence>=70:
+    print("allow to exam")
+   
+
+
+    
+
+    
