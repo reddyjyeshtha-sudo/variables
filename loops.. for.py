3d2ea81a5f9108c24e478fcfc9b435ad19df189f
jyeshtha <reddyjyeshtha@gmail.com>
2026-07-10 19:51:57 +0530

    Add files via upload

---
+++ loops.. for.py
@@ -0,0 +1,73 @@
+'''price=int(input())
+if price == 1200:
+    print("red v")
+elif price == 1000:
+    print("almond")
+elif price == 800:
+    print("choc v")
+elif price == 600:
+    print("butter v")
+else:
+    print("sorry")'''
+
+'''a=[10,20,30,40]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40]
+for i in a:
+    print(i,end=",")'''
+
+'''a=[10,20,30,40]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=(4,5,6,7,8,9)
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={4,5,6,7,8,9}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a="codegnan"
+for i in a:
+    print(i)'''
+
+
+'''a=[3.6,7.5]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=["python","java"]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''fruits=["apple","banana","mango"]
+for i in fruits:
+    print(i.upper(),end=",")'''
+'''a=[5+8j,7+5j]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+a=["true","false"]
+'''for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+
+
+
+
