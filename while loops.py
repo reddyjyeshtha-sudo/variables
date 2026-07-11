ca0c3e7b71d4fb697cac24322b0b657e8d0cec90
jyeshtha <reddyjyeshtha@gmail.com>
2026-07-11 20:09:57 +0530

    Add files via upload

---
+++ while loops.py
@@ -0,0 +1,97 @@
+'''a=10
+while a>1:
+    print(a)'''
+
+'''a=10
+while a>=1:
+    print(a)
+    a=a-1'''
+'''a=10
+while a>=1:
+    a=a-1
+    print(a)'''
+'''a=20
+while a>=5:
+    a=a-1
+print(a)'''
+'''a=30
+while a>2:
+    print(a)
+    a-=1'''
+'''a=5
+    while a<25:
+        print(a)
+        a+=1'''
+
+'''a=1
+while a<=10:
+    print(a)
+    a=a+3'''
+'''for i in range(10):
+    print(i)'''
+'''for i in range(5,15):
+    print(i)'''
+
+'''for i in range(5,15):
+    print(i,end=",")'''
+'''for i in range(2,20,2):
+    print(i,end=",")'''
+
+'''for i in range(5,50,5):
+    print(i,end=",")'''
+
+'''for i in range(0,30,3):
+    print(i,end=",")'''
+    
+'''i=int(input())
+if i in range(91,101):
+    print("grade a")
+elif i in range(81,91):
+    print("grade b")
+elif i  in range(71,81):
+    print("grade c")
+elif i  in range(50,71):
+    print("grade d")
+elif i  in range(0,51):
+    print("fail")'''
+
+
+'''a=20
+while a>5:
+    print(a)
+    a=a-1
+    if a==10:
+        break'''
+
+'''a=30
+while a>2:
+    a=a-1
+    if a==20:
+        break
+    print(a)'''
+
+'''for i in  range(40,65):
+    if i==55:
+        break
+    print(i)'''
+'''a="python"
+for i in (a):
+    if i=="h":
+        break
+    print(i)'''
+
+'''a=15
+while a>3:
+    a=a-1
+    if a==12:
+        continue
+    print(a)'''
+a="python"
+for i in a:
+    if i=="h":
+        continue
+    print(i)
+
+
+
+   
