894f848937871f2ea298ad248193bbf47535808c
jyeshtha <reddyjyeshtha@gmail.com>
2026-07-14 19:54:11 +0530

    Add files via upload

---
+++ list comphrension.py
@@ -0,0 +1,60 @@
+'''a=["codegnan","java","python"]
+a=[i.upper() for i in a]
+print(a)'''
+
+
+'''a=["vja","hyd","vzg"]
+a=[i.title() for i in a]
+print(a)'''
+
+'''a=[1,2,3,5,6,8,12,13]
+a=[i*i for i in a]
+print(a)'''
+
+'''for i in range(16)if i %2==0:
+print(i)'''
+
+'''for i in range(21):
+    print(i)'''
+
+'''fruits=["apple","banana","grapes","kiwi","mango","dragon"]
+b=[i for i in fruits if "a" in i]
+print(b)'''
+
+'''a=[i**2 if i%2==0 else i*5 for i in range(21)]
+print(a)'''
+
+account=100000
+card="c"
+password=1234
+while(true):
+c=int(input())
+if c=="card":
+    print("welcome jyesh")
+    elif:
+        print("invalid card")
+
+        pwd=int(input())
+        if pwd=password:
+             print("1.balance enquiry")
+             print("2.withdraw")
+             print("3.exit")
+    
+        elif:
+            print("invalid pass")
+            option=int(input())
+            if option==1:
+                print("acc balance is:",account)
+            elif option==2:
+                amount=int(input())
+                if amount<=account:
+                    account=account-amount
+                    print("remaining balance is :",account)
+                else:
+                    print("insuffienct balance")
+    
+    
+
+
+
+    
